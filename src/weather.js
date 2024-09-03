4af2dc463aa2fcc36eba1e147bf595d48c0ef29d
Stephanie Anderson <srglover126@gmail.com>
2024-09-03 11:50:53 -0400

    Uploaded weather app project html

---
+++ src/weather.js
@@ -0,0 +1,65 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "dbbf0b0ffte4fa30oaa8d9a8aa2bc032";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+  console.log(apiUrl);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
+}
+
+function displayIcon(icon) {
+  let iconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons=?.png`;
+  let iconElement = document.querySelector(".current-temperature-icon");
+  iconElement.innerHTML = response.data.condition.icon;
+}
